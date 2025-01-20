d1a25ebe9bfbfd2724cb3f2e821134adaeb37dfa
Pranav Rajendraprasad <pranavprasad.r500@gmail.com>
2025-01-20 22:55:01 +0530

    Saving changes

--- PROTOTYPE/mc_cmp/presentation/urls.py
+++ PROTOTYPE/mc_cmp/presentation/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpattenrs = [
+urlpatterns = [
+    path('/login', views.login, name='login'),
+    path('', views.register, name='register'),
+    path('/home_caregiver', views.home_caregiver, name='home_caregiver'),
+    path('/home_elder', views.home_elder, name='home_elder'),
 ]
--- PROTOTYPE/mc_cmp/presentation/views.py
+++ PROTOTYPE/mc_cmp/presentation/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+def home_caregiver(request):
+    return render(request, 'home_caregiver.html')
+
+def home_elder(request):
+    return render(request, 'home_elder.html')
+
+def register(request):
+    return render(request, 'register.html')
